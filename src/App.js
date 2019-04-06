36e042abd497796919919f1f3d82357a5e20692c
aereeeee <dofl5576@gmail.com>
2019-04-06 16:45:27 +0900

    init app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        i app
+      </div>
+    );
+  }
+}
+
+export default App;
