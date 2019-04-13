9265c260ae11b438d1810223cdb3993ec8764f12
aereeeee <dofl5576@gmail.com>
2019-04-13 13:29:20 +0900

    Add Score component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import styles from './Game.module.css'
+import Score from '../Score'
+
 
 const Game = () => (
   <div className={styles.wrapper}> 
-    Game
+    <Score />
   </div>
 )
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Game } from './Game'
 export { default as Heading } from './Heading'
+export { default as Score } from './Score'
